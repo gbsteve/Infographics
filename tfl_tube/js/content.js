5d70015dc9349432b456e16d9261ccf20a9451ca
gbsteve <steve@moobark.com>
2014-07-15 12:43:38 +0100

    Changed to XML parsing

--- tfl_tube/js/content.js
+++ tfl_tube/js/content.js
@@ -1,27 +1,24 @@
 var mydata = {};
 
-mydata.url = "http://cloud.tfl.gov.uk/TrackerNet/LineStatus?method=get.status&format=json&callback=?appid=f29caf68?key=eaab13200e93f44010a46e8c3c26c1c1?";
-
-$.getJSON(mydata.url, function (data) {
-    items = data.response.lines;
-
-    $.each(items, function (index, item) {
-
-        var name = document.createElement("td");
-        var status = document.createElement("td");
-
-        name.innerHTML = item.name;
-        status.innerHTML = item.status;
-
-        name.setAttribute('id', item.name);
-
-        var sclass = item.status.split(",")[0].replace(/ /g, "_");
-        status.setAttribute('class', sclass);
-
-        var row = document.createElement("tr");
-        row.appendChild(name);
-        row.appendChild(status);
-
-        document.getElementById('data').appendChild(row);
-    });
+mydata.url = "http://cloud.tfl.gov.uk/TrackerNet/LineStatus";
+$.ajax({
+    type: "GET",
+    url: "mydata.url",
+    dataType: "xml",
+    success: xmlParser
+   });
 });
+
+function xmlParser(xml){
+  //find every Line and print the status
+  $(xml).find("Line Status").each(function()
+  {
+      (this).find("Line").each(function();
+      $("#data").append("<tr><td>" + $(this).attr("Name") + "<td/>");
+      (this).find("Status").each(function();
+      $("#data").append("<td>" + $(this).attr("Description") + "<td/></tr>");
+
+  });
+
+
+;
